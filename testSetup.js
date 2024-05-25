c6dbc7480f2b946ff79721c8af4f01693f9bf658
Darko Michael Agyeman <agyemanmda@gmail.com>
2024-05-25 23:27:12 +0100

    run formatter

--- testSetup.js
+++ testSetup.js
@@ -1,6 +1,6 @@
-import { afterEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
-import '@testing-library/jest-dom/vitest'
+import { afterEach } from "vitest";
+import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as matchers from "@testing-library/jest-dom/matchers";
@@ -8,4 +8,4 @@ import * as matchers from "@testing-library/jest-dom/matchers";
 // runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
-})
+});
